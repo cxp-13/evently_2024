627528bf10902a68e91dde9b1deb92fa457b1f9b
cxp-13 <1695219012@qq.com>
2024-01-20 15:00:21 +0800

    fix the error with connect db

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -30,7 +30,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
-export const connectToDatabase = async () => {
+ const connectToDatabase = async () => {
     if (cached.conn) return cached.conn;
 
     if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
@@ -42,5 +42,7 @@ export const connectToDatabase = async () => {
     cached.conn = await cached.promise;
     return cached.conn;
 }
+
+export default connectToDatabase
 
 
